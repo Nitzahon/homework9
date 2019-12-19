748a860dfd5b842e47bec7d06283ebb4946bb045
Nitzahon <nadavga@gmail.com>
2019-12-19 23:51:30 +0200

    changed bootstrap colums

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,28 +5,28 @@ export default function Register() {
       
     <div className="reactReg">     
         <div className="row">
-          <div className="col-3"></div>
+          <div className="col-md-8 col-lg-3"></div>
 
           <input
             name="input1"
             type="text"
             placeholder="test1"
-            className="col-3"
+            className="col-md-8 col-lg-3"
           />
 
           <input
             name="input2"
             type="text"
             placeholder="test2"
-            className="col-3"
+            className="col-md-8 col-lg-3"
           />
 
-          <div className="col-3"></div>
+          <div className="col-md-8 col-lg-3"></div>
         </div>
         <div className="row">
-          <div className="col-4"></div>
-          <button className="col-4 btn btn-success">Register</button>
-          <div className="col-4"></div>
+          <div className="col-md-8 col-lg-4"></div>
+          <button className="col-md-8 col-lg-4 btn btn-success">Register</button>
+          <div className="col-md-8 col-lg-4"></div>
         </div>
     </div>
   );
